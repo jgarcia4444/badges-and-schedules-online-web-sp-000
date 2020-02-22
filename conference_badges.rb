3385f016ffe9b266a3b23cf22fed74231ac3d9dc
Jake Garcia <bballwiz4@gmail.com>
2020-02-22 08:03:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}"
 end
+
+def batch_badge_creator(name_arr)
+  badges = []
+  name.each do |name|
+    badges << "Hello, my name is #{name}"
+  end
+  return badges
+end
