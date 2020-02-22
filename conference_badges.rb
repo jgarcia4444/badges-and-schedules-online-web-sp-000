093b7fcb1c225ddcfc1c1df660336ccbdd5dd705
Jake Garcia <bballwiz4@gmail.com>
2020-02-22 08:03:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(name_arr)
   badges = []
   name.each do |name|
-    badges << "Hello, my name is #{name}"
+    badges << "Hello, my name is #{name}."
   end
   return badges
 end
